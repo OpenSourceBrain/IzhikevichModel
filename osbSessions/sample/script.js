4e3f060fb0b4aeaba285ab7e31a32296f2184ed9
Matt Earnshaw <matt@earnshaw.org.uk>
2018-01-25 18:35:52 +0000

    script

--- osbSessions/sample/script.js
+++ osbSessions/sample/script.js
@@ -1,4 +1,2 @@
 initOSBGeppetto("network", net1);
-GEPPETTO.SceneController.addColorFunction(window.getRecordedMembranePotentials(), window.voltage_color);
-window.setupColorbar(window.getRecordedMembranePotentials(), window.voltage_color, false, 'Voltage color scale', 'Membrane Potential (V)', 800, 600);
-//Project.getActiveExperiment().play({step: 10})
+setTimeout(function() {	GEPPETTO.SceneController.addColorFunction(window.getRecordedMembranePotentials(), window.voltage_color); window.setupColorbar(window.getRecordedMembranePotentials(), window.voltage_color, false, 'Voltage color scale', 'Membrane Potential (V)', 800, 600); }, 5000);
