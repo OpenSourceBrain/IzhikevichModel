e4442bacdd76a4a5b400617330afcf2177c96b69
Matt Earnshaw <matt@earnshaw.org.uk>
2018-01-25 17:35:21 +0000

    update

--- osbSessions/sample/script.js
+++ osbSessions/sample/script.js
@@ -1,4 +1,4 @@
 initOSBGeppetto("network", net1);
 GEPPETTO.SceneController.addColorFunction(window.getRecordedMembranePotentials(), window.voltage_color);
-window.setupColorbar(window.getRecordedMembranePotentials(), window.voltage_color, false, 'Voltage color scale', 'Membrane Potential (V)');
+window.setupColorbar(window.getRecordedMembranePotentials(), window.voltage_color, false, 'Voltage color scale', 'Membrane Potential (V)', 800, 600);
 Project.getActiveExperiment().play({step: 10})
