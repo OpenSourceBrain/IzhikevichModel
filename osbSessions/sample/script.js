466e49ea20f633e0c154dabd31bf9868f3bbd4aa
Matt Earnshaw <matt@earnshaw.org.uk>
2018-01-29 14:20:02 +0000

    script

--- osbSessions/sample/script.js
+++ osbSessions/sample/script.js
@@ -1,2 +1,2 @@
 initOSBGeppetto("network", net1);
-setTimeout(function() {	GEPPETTO.SceneController.addColorFunction(window.getRecordedMembranePotentials(), window.voltage_color); window.setupColorbar(window.getRecordedMembranePotentials(), window.voltage_color, false, 'Voltage color scale', 'Membrane Potential (V)', 800, 600); }, 5000);
+setTimeout(function() {	GEPPETTO.SceneController.addColorFunction(window.getRecordedMembranePotentials(), window.voltage_color); window.setupColorbar(window.getRecordedMembranePotentials(), window.voltage_color, false, 'Voltage color scale', 'Membrane Potential (V)', 800, 600); }, 10000);
